35bc64df7ab25de2eaca72fda7635c862f072fc0
Marit Oliemans <maritoliemans@hotmail.com>
2018-05-18 17:15:45 -0600

    Add ingredients

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 class BurgerBuilder extends Component {
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 2,
-            cheese: 1,
-            meat: 1
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
+
+    addIngredientHandler = (type) => {
+        let currentIngredient = this.state.ingredients[type] + 1;
+        let updatedIngredients = {...this.state.ingredients, [type]: currentIngredient}
+        this.setState({ingredients: updatedIngredients})
+    }
+
+
     render(){
         return(
             <React.Fragment>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Controls</div>
+                <BuildControls addIngredient={this.addIngredientHandler}/>
             </React.Fragment>
         )
     }
